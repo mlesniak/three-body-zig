d80058e87ad5398319ee49191259f575242318fa
Michael Lesniak <mail@mlesniak.com>
2024-06-19 20:55:45 +0200

    Refactoring

--- src/main.zig
+++ src/main.zig
@@ -7,35 +7,40 @@ pub fn main() !void {
     const o1 = core.Object{ .pos = .{ .x = 0, .y = 0 }, .vel = .{ .x = 0, .y = 0 }, .acc = core.Vec2.zero(), .mass = 1 };
     const o2 = core.Object{ .pos = .{ .x = 1, .y = 0 }, .vel = .{ .x = 0, .y = 1 }, .acc = core.Vec2.zero(), .mass = 1 };
     const o3 = core.Object{ .pos = .{ .x = 0, .y = 1 }, .vel = .{ .x = -1, .y = 0 }, .acc = core.Vec2.zero(), .mass = 1 };
-    var objects: [3]core.Object = [_]core.Object{ o1, o2, o3 };
+    const objects = [_]core.Object{ o1, o2, o3 };
 
+    // @mlesniak move the simulation to a separate function / struct
+    //           which encapsulates the state, i.e. step, and provides
+    //           a nextStep(delta) function.
+    // @mlesniak Technically, not stateless since we're continously
+    //           updating the object and overwriting their values.
     var step: i64 = 0;
     while (step < 3) : (step += 1) {
         var accs: [3]core.Vec2 = undefined;
         std.debug.print("\nStep {} with delta={}\n", .{ step, delta });
         // Compute new acceleration.
         for (0..objects.len) |i| {
-            accs[i] = computeAccSum(objects, i);
+            accs[i] = computeAccSum(&objects, i);
         }
         for (0..accs.len) |i| {
             const acc = accs[i];
             std.debug.print("accs[{}] = ({}, {})\n", .{ i, acc.x, acc.y });
         }
 
-        // Update velocity and position.
-        for (0..objects.len) |i| {
-            var o = &objects[i];
-            const acc = accs[i];
-            const newVel = o.vel.add(acc.scale(delta));
-            const newPos = o.pos.add(newVel.scale(delta));
-            o.pos = newPos;
-            o.vel = newVel;
-            std.debug.print("objects[{}] = ({}, {}, {})\n", .{ i, newPos.x, newPos.y, newVel.len() });
-        }
+        // // Update velocity and position.
+        // for (0..objects.len) |i| {
+        //     var o = &objects[i];
+        //     const acc = accs[i];
+        //     const newVel = o.vel.add(acc.scale(delta));
+        //     const newPos = o.pos.add(newVel.scale(delta));
+        //     o.pos = newPos;
+        //     o.vel = newVel;
+        //     std.debug.print("objects[{}] = ({}, {}, {})\n", .{ i, newPos.x, newPos.y, newVel.len() });
+        // }
     }
 }
 
-fn computeAccSum(objects: [3]core.Object, current: u64) core.Vec2 {
+fn computeAccSum(objects: []const core.Object, current: u64) core.Vec2 {
     const curObject = objects[current];
     var accSum = core.Vec2.zero();
     for (objects, 0..) |o, i| {
