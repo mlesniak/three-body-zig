92060d262af660655771bb18c8d4531f1417a588
Michael Lesniak <mail@mlesniak.com>
2024-07-01 20:22:15 +0200

    Improvements

--- src/main.zig
+++ src/main.zig
@@ -7,24 +7,6 @@ const ray = @cImport({
 
 // @mlesniak explain units, m/s, kg, ...
 fn initSimulation(alloc: *const std.mem.Allocator) !simulation.Simulation {
-    // const o0 = core.Object{
-    //     .pos = core.Vec2{ .x = 5_000_000_000, .y = 4_000_000_000 },
-    //     .vel = core.Vec2{ .x = -1e4, .y = -1e4 },
-    //     .acc = core.Vec2{ .x = 0, .y = 0 },
-    //     .mass = 1e23,
-    // };
-    // const o1 = core.Object{
-    //     .pos = core.Vec2{ .x = 5_000_000_000, .y = 5_000_000_000 },
-    //     .vel = core.Vec2{ .x = 0, .y = -1e4 },
-    //     .acc = core.Vec2{ .x = 0, .y = 0 },
-    //     .mass = 1e24,
-    // };
-    // const o2 = core.Object{
-    //     .pos = core.Vec2{ .x = 4_000_000_000, .y = 6_500_000_000 },
-    //     .vel = core.Vec2{ .x = 3e5, .y = 1e4 },
-    //     .acc = core.Vec2{ .x = 0, .y = 0 },
-    //     .mass = 1e22,
-    // };
     const o0 = core.Object{
         .pos = .{ .x = 3_000_000_000, .y = 5_000_000_000 },
         .vel = .{ .x = 0, .y = 0 },
@@ -50,17 +32,17 @@ fn initSimulation(alloc: *const std.mem.Allocator) !simulation.Simulation {
     };
     const o3 = core.Object{
         .pos = .{ .x = 5_000_000_000, .y = 6_000_000_000 },
-        .vel = .{ .x = 8e3, .y = 0 },
+        .vel = .{ .x = 8e3, .y = 1e3 },
         .acc = core.Vec2.zero(),
         .mass = 1e24,
-        .color = ray.YELLOW
+        .color = ray.YELLOW,
     };
     const o4 = core.Object{
         .pos = .{ .x = 7_000_000_000, .y = 2_000_000_000 },
-        .vel = .{ .x = 0, .y = 0 },
+        .vel = .{ .x = -5e3, .y = 0 },
         .acc = core.Vec2.zero(),
-        .mass = 1e15,
-        .color = ray.RED
+        .mass = 1e18,
+        .color = ray.PURPLE,
     };
 
     var objects = try alloc.alloc(core.Object, 5);
@@ -69,7 +51,6 @@ fn initSimulation(alloc: *const std.mem.Allocator) !simulation.Simulation {
     objects[2] = o2;
     objects[3] = o3;
     objects[4] = o4;
-    // objects[2] = o3;
     const sim = simulation.Simulation.init(objects);
     return sim;
 }
@@ -81,16 +62,15 @@ pub fn main() !void {
     const width = 1000;
     const height = 1000;
 
+    ray.SetConfigFlags(ray.FLAG_MSAA_4X_HINT);
     ray.InitWindow(width, height, "Three body problem - simulation");
     defer ray.CloseWindow();
     ray.SetTargetFPS(60);
 
-    // @mlesniak idea simulate N steps per frame for more numerical precision
-    //           and have a smaller dt.
     const dt: f64 = 250;
+    const stepsPerFrame: i32 = 10;
 
     const zoomFactor: f64 = 10_000_000;
-    var advance: bool = true;
 
     // @mlesniak Antialising? background color? ...
     var i: i32 = 0;
@@ -101,33 +81,24 @@ pub fn main() !void {
         ray.ClearBackground(ray.BLACK);
 
         for (sim.objects) |o| {
-            // std.debug.print("x positions\n", .{});
-            // std.debug.print("{}\n", .{o.pos.x});
-            // const f = o.pos.x / zoomFactor;
-            const x: i32 = @intFromFloat(o.pos.x / zoomFactor);
-            const y: i32 = @intFromFloat(o.pos.y / zoomFactor);
-            // if (i == 0) {
-            //     std.debug.print("{}, {}, {}, {}\n", .{o.pos.x, x, y, f});
-            // }
             for (o.trail) |tp| {
                 const tx: i32 = @intFromFloat(tp.x / zoomFactor);
                 const ty: i32 = @intFromFloat(tp.y / zoomFactor);
                 ray.DrawCircle(tx, ty, 1, o.color);
             }
+        }
+
+        for (sim.objects) |o| {
+            const x: i32 = @intFromFloat(o.pos.x / zoomFactor);
+            const y: i32 = @intFromFloat(o.pos.y / zoomFactor);
             ray.DrawCircle(x, y, 10, o.color);
         }
 
-        if (ray.IsKeyDown(ray.KEY_J)) {
-            advance = true;
-        }
-
-        if (advance) {
-            // advance = false;
-            sim.step(dt);
-            // for (sim.objects, 0..sim.objects.len) |o, idx| {
-            //     std.debug.print("[{}] {}\n", .{ idx, o });
-            // }
-            // std.debug.print("\n", .{});
+        {
+            var s: i32 = 0;
+            while (s < stepsPerFrame) : (s += 1) {
+                sim.step(dt);
+            }
         }
 
         i += 1;
